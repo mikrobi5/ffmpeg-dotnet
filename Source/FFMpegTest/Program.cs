654e425262d6f149e4c3239730dc4c215c930193
Christian Telle <christian.telle@gmx.de>
2020-01-13 17:20:01 +0100

    test commit

---
+++ Source/FFMpegTest/Program.cs
@@ -0,0 +1,15 @@
+﻿using FFmpegDotNet;
+using System;
+
+namespace FFMpegTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            var test = new Initialize();
+
+        }
+    }
+}
